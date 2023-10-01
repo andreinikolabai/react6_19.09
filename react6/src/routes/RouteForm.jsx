79728c71748217ef64000d4c9d4b46900dc6ab94
andreinikolabai <andreiplus2000@gmail.com>
2023-10-01 23:34:14 +0300

    update

--- react6/src/routes/RouteForm.jsx
+++ react6/src/routes/RouteForm.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import CountryForm from "../components/CountryForm/CountryForm";
-import useTodo from "./../hooks/useTodo"; // Импортируйте useTodo
+import CountryForm from '../components/CountryForm/CountryForm';
 
 export default function RouteForm() {
-    const { state, handleItemDelete } = useTodo(); // Используйте useTodo
-
     return (
         <>
-            <CountryForm countries={state.todo} onFormSubmit={handleItemDelete} />
+            <CountryForm />
         </>
     );
 }
