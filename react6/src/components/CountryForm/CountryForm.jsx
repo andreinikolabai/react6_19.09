7ccb68c71676a899966e38de618c040454f633d2
andreinikolabai <andreiplus2000@gmail.com>
2023-10-02 19:05:57 +0300

    update

--- react6/src/components/CountryForm/CountryForm.jsx
+++ react6/src/components/CountryForm/CountryForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './style.sass';
 import { useNavigate } from 'react-router-dom';
 
 export default function CountryForm({ countries }) {
@@ -30,6 +31,7 @@ export default function CountryForm({ countries }) {
     return (
         <form onSubmit={handleSubmit}>
             <div>
+                <h3>Capital Form Component</h3>
                 <label>Select a capital:</label>
                 <select value={selectedCapital} onChange={handleCapitalChange}>
                     <option value="">Select a capital</option>
@@ -40,8 +42,8 @@ export default function CountryForm({ countries }) {
                     ))}
                 </select>
             </div>
-            {selectedCapital && (
                 <div>
+                    <h4>Select Translation Language:</h4>
                     <label>Select a translation:</label>
                     <select value={selectedTranslation} onChange={handleTranslationChange}>
                         <option value="">Select a translation</option>
@@ -52,65 +54,9 @@ export default function CountryForm({ countries }) {
                         ))}
                     </select>
                 </div>
-            )}
-            {selectedCapital && selectedTranslation && (
                 <div>
                     <button>Read more about {selectedCapital}</button>
                 </div>
-            )}
         </form>
     );
 }
-
-// import React, { useState } from "react";
-// import './style.sass'
-//
-// export default function CountryForm({ countries, onFormSubmit }) {
-//     const [selectedCapital, setSelectedCapital] = useState(countries[0].capital);
-//     const [selectedTranslation, setSelectedTranslation] = useState(countries[0].translations[0]);
-//     const selectedCountry = countries.find((country) => country.capital === selectedCapital);
-//     const selectedCountryName = selectedCountry ? selectedCountry.country : "";
-//
-//     const handleCapitalChange = (e) => {
-//         setSelectedCapital(e.target.value);
-//     };
-//
-//     const handleTranslationChange = (e) => {
-//         setSelectedTranslation(e.target.value);
-//     };
-//
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         onFormSubmit(selectedCapital, selectedTranslation);
-//     };
-//
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <div>
-//                 <h3>Capital Form Component</h3>
-//                 <label htmlFor="capitalSelect"></label>
-//                 <h4>Select Country Capital:</h4>
-//                 <select id="capitalSelect" onChange={handleCapitalChange} value={selectedCapital}>
-//                     {countries.map((country, index) => (
-//                         <option key={index} value={country.capital}>
-//                             {`${country.flag} ${country.capital}`}
-//                         </option>
-//                     ))}
-//                 </select>
-//             </div>
-//             <div>
-//                 <label htmlFor="translationSelect"></label>
-//                 <h4>Select Translation Language:</h4>
-//                 <select id="translationSelect" onChange={handleTranslationChange} value={selectedTranslation}>
-//                     {selectedCountry &&
-//                         selectedCountry.translations.map((translation, index) => (
-//                             <option key={index} value={translation}>
-//                                 {translation}
-//                             </option>
-//                         ))}
-//                 </select>
-//             </div>
-//             <button type="submit">Read more about {selectedCountryName}</button>
-//         </form>
-//     );
-// }
