84c48c955aa9079ae90be5fe16e8ac67896e19d1
andreinikolabai <andreiplus2000@gmail.com>
2023-10-03 00:13:07 +0300

    update

--- react6/src/components/CountryForm/CountryForm.jsx
+++ react6/src/components/CountryForm/CountryForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import './style.sass';
 import { useNavigate } from 'react-router-dom';
 import TodoContext from '../../contexts/todoContext';
@@ -8,6 +8,14 @@ export default function CountryForm() {
     const [selectedTranslation, setSelectedTranslation] = useState('');
     const navigate = useNavigate();
     const todo = useContext(TodoContext);
+
+    // Установите начальные значения для selectedCapital и selectedTranslation
+    useEffect(() => {
+        if (todo.state.countries && todo.state.countries.length > 0) {
+            setSelectedCapital(todo.state.countries[0].capital);
+            setSelectedTranslation(todo.state.countries[0].translations[0]);
+        }
+    }, [todo.state.countries]);
 
     const handleCapitalChange = (event) => {
         const selectedCapital = event.target.value;
@@ -34,8 +42,8 @@ export default function CountryForm() {
         <form onSubmit={handleSubmit}>
             <div>
                 <h3>Capital Form Component</h3>
+                <h4>Select Capital</h4>
                 <select value={selectedCapital} onChange={handleCapitalChange}>
-                    <option value="">Select a capital</option>
                     {todo.state.countries && todo.state.countries.map((country) => (
                         <option key={country.id} value={country.capital}>
                             {`${country.flag} ${country.capital}`}
@@ -46,7 +54,6 @@ export default function CountryForm() {
             <div>
                 <h4>Select Translation Language:</h4>
                 <select value={selectedTranslation} onChange={handleTranslationChange}>
-                    <option value="">Select a translation</option>
                     {selectedCapital && todo.state.countries.find((country) => country.capital === selectedCapital).translations.map((translation) => (
                         <option key={translation} value={translation}>
                             {translation}
--- react6/src/components/CountryList/CountryList.jsx
+++ react6/src/components/CountryList/CountryList.jsx
@@ -1,21 +1,30 @@
 import React, { useContext } from "react";
 import TodoContext from "../../contexts/todoContext";
 import { Link } from 'react-router-dom'
+import './style.sass'
 
 export default function TodoList() {
     const { state, handleItemDelete } = useContext(TodoContext);
 
     return state.countries.length ? (
-        <ul>
-            {state.countries.map((item, index) => (
-                <li key={item.id}>
-                    {item.country}{" "}
-                    <Link to={String(item.id)}>Read more</Link>
-                    <button onClick={() => {
-                        handleItemDelete(item.id);
-                    }}>Delete</button>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <h3>
+                <text>Countries List</text>
+            </h3>
+            <ul className="todo-list">
+                <div>
+                    {state.countries.map((item, index) => (
+                        <li key={item.id}>
+                            {item.country}{" "}
+                            <Link to={String(item.id)}>Read more</Link>
+                            <button onClick={() => {
+                                handleItemDelete(item.id);
+                            }}>Delete
+                            </button>
+                        </li>
+                    ))}
+                </div>
+            </ul>
+        </div>
     ) : null;
 }
